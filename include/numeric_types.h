2a4b09bd97d74f5a32d203021305e77391ae34e7
Noah Orensa <noahorensa@gmail.com>
2021-12-06 08:41:36 -0600

    add numeric_types

---
+++ include/numeric_types.h
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2021 Noah Orensa.
+ * Licensed under the MIT license. See LICENSE file in the project root for details.
+*/
+
+#pragma once
+
+typedef unsigned char uint8;
+typedef unsigned short uint16;
+typedef unsigned int uint32;
+typedef unsigned long uint64;
+
+typedef char int8;
+typedef short int16;
+typedef int int32;
+typedef long int64;
+
+typedef float float32;
+typedef double float64;
+typedef long double float128;
