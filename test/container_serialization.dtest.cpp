f6fce2078739137fbd1d96dea5ce50066caae2a3
Noah Orensa <noahorensa@gmail.com>
2022-05-17 10:01:16 -0600

    add copyright note to test files

--- test/container_serialization.dtest.cpp
+++ test/container_serialization.dtest.cpp
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2021 Noah Orensa.
+ * Licensed under the MIT license. See LICENSE file in the project root for details.
+*/
+
 #include <dtest.h>
 #include "test_serializers.cpp"
 #include "test_serializable.cpp"
--- test/tcp_socket.dtest.cpp
+++ test/tcp_socket.dtest.cpp
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2021 Noah Orensa.
+ * Licensed under the MIT license. See LICENSE file in the project root for details.
+*/
+
 #include <dtest.h>
 #include <tcp_socket.h>
 #include "test_serializable.cpp"
