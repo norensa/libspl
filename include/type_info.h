eb08abdd5a2745fe8a3ac26750fee2e6e7484c1e
Noah Orensa <noahorensa@gmail.com>
2022-12-06 16:58:05 -0600

    add type_info

---
+++ include/type_info.h
@@ -0,0 +1,133 @@
+/*
+ * Copyright (c) 2021-2022 Noah Orensa.
+ * Licensed under the MIT license. See LICENSE file in the project root for details.
+*/
+
+#pragma once
+
+#include <typeinfo>
+#include <string>
+
+namespace spl {
+
+/**
+ * @brief An enhanced type info class that is copy-constructible and
+ * copy-assignable. This class is constructible from and compatible with
+ * std::type_info
+ */
+class TypeInfo {
+
+private:
+
+    size_t _hashCode;
+    std::string _name;
+
+public:
+
+    /**
+     * @brief Construct a new Type Info object
+     */
+    TypeInfo()
+    :   _hashCode(0),
+        _name("")
+    { }
+
+    /**
+     * @brief Construct a new Type Info object
+     * 
+     * @param type A std::type_info object
+     */
+    TypeInfo(const std::type_info &type)
+    :   _hashCode(type.hash_code()),
+        _name(type.name())
+    { }
+
+    /**
+     * @return A unique implementation-specific hash code for this type.
+     */
+    size_t hashCode() const {
+        return _hashCode;
+    }
+
+    /**
+     * @return A unique implementation-specific name string for this type.
+     */
+    const char * name() const {
+        return _name.c_str();
+    }
+
+    bool operator==(const TypeInfo &rhs) const {
+        return _hashCode == rhs._hashCode && _name == rhs._name;
+    }
+
+    bool operator==(const std::type_info &rhs) const {
+        return _hashCode == rhs.hash_code() && _name == rhs.name();
+    }
+
+    bool operator!=(const TypeInfo &rhs) const {
+        return ! operator==(rhs);
+    }
+
+    bool operator!=(const std::type_info &rhs) const {
+        return ! operator==(rhs);
+    }
+
+    bool operator<(const TypeInfo &rhs) const {
+        return _name < rhs._name;
+    }
+
+    bool operator<(const std::type_info &rhs) const {
+        return _name < rhs.name();
+    }
+
+    bool operator<=(const TypeInfo &rhs) const {
+        return _name <= rhs._name;
+    }
+
+    bool operator<=(const std::type_info &rhs) const {
+        return _name <= rhs.name();
+    }
+
+    bool operator>(const TypeInfo &rhs) const {
+        return _name > rhs._name;
+    }
+
+    bool operator>(const std::type_info &rhs) const {
+        return _name > rhs.name();
+    }
+
+    bool operator>=(const TypeInfo &rhs) const {
+        return _name >= rhs._name;
+    }
+
+    bool operator>=(const std::type_info &rhs) const {
+        return _name >= rhs.name();
+    }
+
+    friend bool operator==(const std::type_info &lhs, const TypeInfo &rhs) {
+        return rhs == lhs;
+    }
+
+    friend bool operator!=(const std::type_info &lhs, const TypeInfo &rhs) {
+        return rhs != lhs;
+    }
+
+    friend bool operator<(const std::type_info &lhs, const TypeInfo &rhs) {
+        return rhs > lhs;
+    }
+
+    friend bool operator<=(const std::type_info &lhs, const TypeInfo &rhs) {
+        return rhs >= lhs;
+    }
+
+    friend bool operator>(const std::type_info &lhs, const TypeInfo &rhs) {
+        return rhs < lhs;
+    }
+
+    friend bool operator>=(const std::type_info &lhs, const TypeInfo &rhs) {
+        return rhs <= lhs;
+    }
+
+};
+
+}
---
+++ test/type_info.dtest.cpp
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2021-2022 Noah Orensa.
+ * Licensed under the MIT license. See LICENSE file in the project root for details.
+*/
+
+#include <dtest.h>
+#include <type_info.h>
+
+using namespace spl;
+
+unit("type-info", "default-constructor")
+.body([] {
+    auto t = TypeInfo();
+});
+
+unit("type-info", "constructor")
+.body([] {
+    auto t = TypeInfo(typeid(int));
+});
+
+unit("type-info", "name")
+.body([] {
+    auto t = TypeInfo(typeid(int));
+
+    assert(strcmp(t.name(), typeid(int).name()) == 0);
+});
+
+unit("type-info", "hashCode")
+.body([] {
+    auto t = TypeInfo(typeid(int));
+
+    assert(t.hashCode() == typeid(int).hash_code());
+});
+
+unit("type-info", "equality")
+.body([] {
+    auto t = TypeInfo(typeid(int));
+
+    assert(t == typeid(int));
+    assert(typeid(int) == t);
+});
